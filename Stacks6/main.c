1b0b0e5a5a35a4db96afafc21e855f5bbf974ac6
Nakseung Choi <chlskrtmd123@gmail.com>
2022-06-25 01:29:18 -0700

    errors fixed

--- Stacks6/main.c
+++ Stacks6/main.c
@@ -1,95 +1,59 @@
 /**
  *  main.c
- *  @brief currently the code has an error.
+ *  @brief Infix to postfix
  */
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdbool.h>
-#define MAX_SIZE 101
+#include<stdio.h>
+#include<ctype.h>
 
-char A[MAX_SIZE];
-int top = -1; // empty stack if -1.
+char stack[100];
+int top = -1;
 
-void Push(char x){
-    if(top == MAX_SIZE -1){
-        printf("Erorr: stack overflow\n");
-        return;
-    }
-    A[++top] = x; // This is equal to top++; and A[top] = x;
-
+void push(char x){
+    stack[++top] = x;
 }
-void Pop(){
-    if(top == -1){
-        printf("Error: No element to pop\n");
-        return;
-    }
-    top--;
+char pop(){
+    if(top == -1)
+        return -1;
+    else
+        return stack[top--];
 }
-char Top(){
-    return A[top];
+int priority(char x){
+    if(x == '(')
+        return 0;
+    if(x == '+' || x == '-')
+        return 1;
+    if(x == '*' || x == '/')
+        return 2;
+    return 0;
 }
 
+int main(){
+    char exp[100];
+    char *e, x;
+    printf("Enter the expression : ");
+    scanf("%s",exp);
+    printf("\n");
+    e = exp;
 
-bool isOperator(char x[])
-{
-    printf("%c\n", x);
-    switch(x){
-        case '+':
-            return true;
-        case '-':
-            return true;
-        case '/':
-            return true;
-        case '*':
-            return true;
-    }
-    return false;
-}
-
-// Convert prefix to Postfix expression
-char preToPost(char pre_exp[])
-{
-
-    // length of expression
-    int length = strlen(pre_exp);
-    // reading from right to left
-    for(int i = length - 1; i >= 0; i--)
-    {
-        // check if symbol is operator
-        if(isOperator(pre_exp[i]))
-        {
-            // pop two operands from stack
-            char op1 = Top();
-            printf("hey: %c\n", op1);
-            Pop();
-            char op2 = Top();
-            //printf("hey: %s", op2);
-            Pop();
-
-            // concat the operands and operator
-            char temp = op1 + op2 + pre_exp[i];
-
-            // Push string temp back to stack
-            Push(temp);
+    while(*e != '\0'){
+        if(isalnum(*e))
+            printf("%c ",*e);
+        else if(*e == '(')
+            push(*e);
+        else if(*e == ')'){
+            while((x = pop()) != '(')
+                printf("%c ", x);
+        }else{
+            while(priority(stack[top]) >= priority(*e))
+                printf("%c ",pop());
+            push(*e);
         }
-
-        // if symbol is an operand
-        else {
-
-            // push the operand to the stack
-            Push(pre_exp[i]);
-        }
+        e++;
     }
 
-    // stack contains only the Postfix expression
-    return Top();
-}
-
-// Driver Code
-int main()
-{
-    char pre_exp[] = "*-A/BC-/AKL";
-    printf("Postfix: %s", preToPost(pre_exp));
+    while(top != -1){
+        printf("%c ",pop());
+    }
     return 0;
 }
