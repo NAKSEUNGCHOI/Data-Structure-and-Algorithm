a60df1aca43b455276ed7b0a6352f57bcbc939ac
Nakseung Choi <chlskrtmd123@gmail.com>
2022-06-26 12:56:23 -0700

    binary search tree implemented

--- Binary Tree1/main.c
+++ Binary Tree1/main.c
@@ -1,10 +1,11 @@
 /**
  * main.c
- * @brief binary tree: each node can have only two children at most.
- * strict/proper binary tree: each node can have either 2 or 0 children.
- * complete binary tree: all levels except possibly the last are completely filled and all nodes
+ * @brief
+ * 1. binary tree: each node can have only two children at most.
+ * 2. strict/proper binary tree: each node can have either 2 or 0 children.
+ * 3. complete binary tree: all levels except possibly the last are completely filled and all nodes
  * are as left as possible
- * perfect binary tree: maximum No. of nodes in a binary tree with height, "h"
+ * 4. perfect binary tree: maximum No. of nodes in a binary tree with height, "h"
  * = 2^0 + 2^1 + 2^2 + .... + 2^h
  * = 2^(h+1) -1
  * = 2^(No. of levels) - 1
@@ -33,21 +34,120 @@
  *
  * array is also used for heaps.
  *
+ * 5. Binary Search Tree: left index is lesser and the right index is greater than the parent node.
  *
- *                  O                           O                        O
- *                /   \                       /   \                   /     \
- *               O     O                     O     O                 O       O
- *              / \   / \                   / \   / \               / \     / \
- *             O   O O   O                 O   O O   O             O   O   O   O
- *                / \                     / \                     / \ / \ / \ / \
- *               O   O                   O   O                   O  O O O O O O  O
- *          Strict Binary Tree         Complete Binary Tree     Perfect Binary Tree
+ *                  O                        O                        O
+ *                /   \                    /   \                   /     \
+ *               O     O                  O     O                 O       O
+ *              / \   / \                / \   / \               / \     / \
+ *             O   O O   O              O   O O   O             O   O   O   O
+ *                / \                  / \                     / \ / \ / \ / \
+ *               O   O                O   O                   O  O O O O O O  O
+ *          Strict Binary Tree      Complete Binary Tree     Perfect Binary Tree
+ *
+ *                  15
+ *                /    \
+ *              12      20
+ *             /  \    /  \
+ *            8   13  17  22
+ *          Binary Search Tree
+ *
+ *
+ *
  */
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
-int main()
-{
-    printf("Hello world!\n");
-    return 0;
+struct Node{
+    int data;
+    struct Node* left;
+    struct Node* right;
+};
+
+struct Node* newNode(int data){
+    struct Node* temp = (struct Node*)malloc(sizeof(struct Node*));
+    temp->data = data;
+    temp->left = temp->right = NULL;
+    return temp;
+}
+struct Node* minValueNode(struct Node* node){
+    struct Node* current = node;
+    while( current && current->left != NULL) current = current->left;
+    return current;
+};
+
+struct Node* Insert(struct Node* root, int data){
+    if(root == NULL){ // empty tree
+        root = newNode(data);
+        return root;
+    }else if(data <= root->data){
+        root->left = Insert(root->left, data);
+    }else{
+        root->right = Insert(root->right, data);
+    }
+    return root;
+}
+struct Node* Delete(struct Node* root, int data){
+    if(root == NULL) return root; //return if empty
+    if(data < root->data) root->left = Delete(root->left, data);
+    else if(data > root->data) Delete(root->right, data);
+    else{
+        if(root->left == NULL){
+            struct Node* temp = root->right;
+            free(root);
+            return temp;
+        }else if(root->right == NULL){
+            struct Node* temp = root->left;
+            free(root);
+            return temp;
+        }
+        struct Node* temp = minValueNode(root->right);
+        root->data = temp->data;
+        root->right = Delete(root->right, temp->data);
+    }
+    return root;
+}
+
+void inOrder(struct Node* root){
+    if(root != NULL) {
+        inOrder(root->left);
+        printf("%d \n", root->data);
+        inOrder(root->right);
+    }
+}
+
+
+int main(){
+    struct Node* root = NULL;
+    root = Insert(root, 25);
+    int data, value;
+    while(1){
+        data = 0;
+        printf("1. Insert Operation\n");
+        printf("2. Delete Operation\n");
+        printf("3. Search Operation\n");
+        printf("4. Exit Operation\n");
+        printf("Enter a number: ");
+        scanf("%d", &value);
+        switch(value){
+            case 1:
+                printf("Enter the number to be inserted in binary tree: ");
+                scanf("%d", &data);
+                Insert(root, data);
+                break;
+            case 2:
+                printf("Enter the number to be deleted from binary tree: ");
+                scanf("%d", &data);
+                Delete(root, data);
+                break;
+            case 3:
+                inOrder(root);
+                break;
+            case 4:
+                exit(0);
+            default:
+                printf("Incorrect Operation. Try again.");
+        }
+    }
 }
