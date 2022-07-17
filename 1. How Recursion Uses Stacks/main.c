29ba6f13de2a7297924adfdcc52b316c43bba2c0
Nakseung Choi <chlskrtmd123@gmail.com>
2022-07-17 11:28:03 -0700

    initial commit

--- 1. How Recursion Uses Stacks/main.c
+++ 1. How Recursion Uses Stacks/main.c
@@ -5,42 +5,14 @@
  *  main.c
  *  @brief How Recursion Uses Stack.
  *  Time Complexity: O(n) The time taken is equal to the number of x.
- *  Space Complexity: O(n) n is created in stack section of the memory n number of times.
- *  Space Complexity of iterative way: O(1) n is called once.
  */
 
-/**
- *  void fun1(int x)
- *  @brief Tail recursion.
- */
 void fun1(int x){                   // T(n)
     if(x > 0){                      // 1
         printf("%d ", x);           // 1
         fun1(x-1);                  // T(n-1)
     }
-}
-/**
- *  void iterate(int x)
- *  @brief iterative way corresponding to tail recursion above.
- */
-void iterative1(int x){
-    while(x > 0){
-        printf("%d ", x);
-        x--;
-    }
-}
-void iterative2(int x){
-    int i = 1;
-    while(i <= n){
-        printf("%d ", i);
-        i++;
-
-    }
-}
-/**
- *  void fun2(int x)
- *  @brief Head recursion.
- */                                 // T(n) = T(n-1) + 1      when n > 0 ---------------- 1
+}                                   // T(n) = T(n-1) + 1      when n > 0 ---------------- 1
 void fun2(int x){                   // T(n) = 1               when n = 0
     if(x > 0){                      // if T(n) = T(n-1) + 1
         fun2(x-1);                  // T(n-1) = T(n-2) + 1               ---------------- 2
@@ -52,10 +24,6 @@ int main(){                         // Assuming that n-k = 0, n = k
     int x = 3;                      // T(n) = T(n-n) + n
     fun1(x);                        // T(n) = T(0) + n
     printf("\n");                   // T(n) = 1 + n = O(n)
-    iterative1(x);
-    printf("\n");
-
     fun2(x);
     printf("\n");
-    iterative2(x);
 }
--- 2. Static Variable in Recursion/main.c
+++ 2. Static Variable in Recursion/main.c
@@ -1,12 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/**
- *  main.c
- *  @brief the static variable is created in code section of the memory.
- *  This variable gets created once, so it is same as creating it as a global variable.
- *  This is more beneficial b/c all of the functions can share it.
- */
 
 int fun(int n){
     static int x = 0;
@@ -20,7 +14,6 @@ int fun(int n){
 
 int main()
 {
-    int r, x = 5;
-    r = fun(x);
-    printf("%d ", r);
+    int x = 5;
+    printf("%d ", fun(x));
 }
